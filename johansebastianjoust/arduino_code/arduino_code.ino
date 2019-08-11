1e071c0c54aa7e409556dc264d7fe82a9147894d
erikhallin <erik-hallin@hotmail.com>
2019-08-11 19:15:01 +0200

    Create arduino_code.ino

---
+++ johansebastianjoust/arduino_code/arduino_code.ino
@@ -0,0 +1,61 @@
+bool alive=true;
+const int buzzer = 6;
+const int ledr=9;
+const int ledg=10;
+const int ledb=11;
+
+void setup() 
+{
+  //pinMode(buzzer, OUTPUT); 
+  pinMode(ledr, OUTPUT); 
+  pinMode(ledg, OUTPUT); 
+  pinMode(ledb, OUTPUT); 
+  
+  //Init RGB, set color
+
+  //Summer
+
+  //Acc
+
+  //temp output
+
+}
+
+void loop() 
+{
+  /*tone(buzzer, 1000);   // set the LED on
+  delay(1000);              // wait for a second
+  noTone(buzzer);    // set the LED off
+  delay(1000);              // wait for a second*/
+
+  /*analogWrite(ledr, 150);
+  analogWrite(ledg, 0);
+  analogWrite(ledb, 250);
+  delay(1000);
+  analogWrite(ledr, 250);
+  analogWrite(ledg, 100);
+  analogWrite(ledb, 100);
+  delay(1000);*/
+  
+  /*if(alive)
+  {
+    //check Acc input
+  
+    //if not alive
+    {
+      alive=false;
+      
+      //set LED color
+      {
+
+      }
+      
+
+      //Summer beep
+      {
+
+      }
+    }
+  }*/
+
+}
